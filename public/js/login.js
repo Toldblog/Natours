44c3464a0c4071390e7de663feaee0b40c0bf55e
Toldblog <tophuochung@gmail.com>
2023-12-02 20:32:27 +0700

    update

--- public/js/login.js
+++ public/js/login.js
@@ -3,17 +3,16 @@ import axios from 'axios';
 import { showAlert } from './alerts';
 
 export const login = async (email, password) => {
-  // using axios to connect with backend by sending method, url and data
   try {
     const res = await axios({
       method: 'POST',
       url: 'http://127.0.0.1:3000/api/v1/users/login',
       data: {
         email,
-        password,
-      },
+        password
+      }
     });
-    console.log(res);
+
     if (res.data.status === 'success') {
       showAlert('success', 'Logged in successfully!');
       window.setTimeout(() => {
@@ -29,7 +28,7 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/v1/users/logout',
+      url: 'http://127.0.0.1:3000/api/v1/users/logout'
     });
     if ((res.data.status = 'success')) location.reload(true);
   } catch (err) {
